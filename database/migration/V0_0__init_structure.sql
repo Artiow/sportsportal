9d91d6d056b9d8f51d38b95680394c99a4d2fae2
Artiow <namednev_a@mail.ru>
2018-07-20 00:18:22 +0300

    db structure template add

---
+++ database/migration/V0_0__init_structure.sql
@@ -0,0 +1,51 @@
+-----------------------------------
+-- role
+
+create table common.role
+(
+  id          serial      not null
+    constraint role_pk
+    primary key,
+
+  code        varchar(45) not null,
+  name        varchar(45) not null,
+  description varchar(90)
+);
+
+create unique index role_code_uindex
+  on common.role (code);
+
+create unique index role_name_uindex
+  on common.role (name);
+
+-----------------------------------
+-- user
+
+create table common."user"
+(
+  id       serial      not null
+    constraint user_pk
+    primary key,
+
+  login    varchar(45) not null,
+  password varchar(90) not null
+);
+
+create unique index user_login_uindex
+  on common."user" (login);
+
+-- authority
+
+create table common.authority
+(
+  user_id integer not null
+    constraint authority_user_id_fk
+    references common."user",
+
+  role_id integer not null
+    constraint authority_role_id_fk
+    references common.role,
+
+  constraint authority_pk
+  primary key (user_id, role_id)
+);
