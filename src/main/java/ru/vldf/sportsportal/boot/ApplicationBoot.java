4fb9663ed3b83e25d1b67c938eb78ded02d82c21
Artiow <namednev_a@mail.ru>
2018-07-19 23:26:39 +0300

    init commit

---
+++ src/main/java/ru/vldf/sportsportal/boot/ApplicationBoot.java
@@ -0,0 +1,37 @@
+package ru.vldf.sportsportal.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EnableTransactionManagement
+@EntityScan({
+        "ru.vldf.sportsportal.domain"
+})
+@EnableJpaRepositories({
+        "ru.vldf.sportsportal.repository"
+})
+@ComponentScan({
+        "ru.vldf.sportsportal.dto",
+        "ru.vldf.sportsportal.mapper",
+        "ru.vldf.sportsportal.config",
+        "ru.vldf.sportsportal.service",
+        "ru.vldf.sportsportal.controller"
+})
+public class ApplicationBoot extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationBoot.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ApplicationBoot.class);
+    }
+}
---
+++ src/test/java/ru/vldf/sportsportal/ApplicationBootTests.java
@@ -0,0 +1,19 @@
+package ru.vldf.sportsportal;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import ru.vldf.sportsportal.boot.ApplicationBoot;
+
+@Ignore
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ApplicationBoot.class)
+public class ApplicationBootTests {
+
+    @Test
+    public void contextLoads() {
+
+    }
+}
