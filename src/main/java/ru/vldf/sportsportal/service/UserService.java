37f8d48151c703a4ebec4c86a0ee1eda93c61355
Artiow <namednev_a@mail.ru>
2018-08-16 08:33:25 +0300

    abstract security service add, some static resource add

--- src/main/java/ru/vldf/sportsportal/service/UserService.java
+++ src/main/java/ru/vldf/sportsportal/service/UserService.java
@@ -136,9 +136,7 @@ public class UserService {
     public Integer register(@NotNull UserDTO userDTO) throws ResourceCannotCreateException {
         String login = userDTO.getLogin();
         if (userRepository.existsByLogin(login)) {
-            throw new ResourceCannotCreateException(messages.getAndFormat(
-                    "sportsportal.User.alreadyExistByLogin.message", login
-            ));
+            throw new ResourceCannotCreateException(messages.getAndFormat("sportsportal.User.alreadyExistByLogin.message", login));
         }
 
         UserEntity user = userMapper.toEntity(userDTO.setPassword(passwordEncoder.encode(userDTO.getPassword())));
---
+++ src/main/java/ru/vldf/sportsportal/service/generic/AbstractSecurityService.java
@@ -0,0 +1,64 @@
+package ru.vldf.sportsportal.service.generic;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import ru.vldf.sportsportal.config.messages.MessageContainer;
+import ru.vldf.sportsportal.domain.sectional.common.UserEntity;
+import ru.vldf.sportsportal.repository.RoleRepository;
+import ru.vldf.sportsportal.repository.UserRepository;
+import ru.vldf.sportsportal.service.security.userdetails.IdentifiedUserDetails;
+
+public abstract class AbstractSecurityService {
+
+    private MessageContainer messages;
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+
+    protected MessageContainer getMessages() {
+        return messages;
+    }
+
+    protected void setMessages(MessageContainer messages) {
+        this.messages = messages;
+    }
+
+    protected UserRepository getUserRepository() {
+        return userRepository;
+    }
+
+    protected void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    protected RoleRepository getRoleRepository() {
+        return roleRepository;
+    }
+
+    protected void setRoleRepository(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+
+    /**
+     * Returns authenticated user id.
+     *
+     * @return user id
+     * @throws AuthorizationRequiredException - if user is anonymous
+     */
+    protected Integer getCurrentUserId() throws AuthorizationRequiredException {
+        try {
+            return ((IdentifiedUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getId();
+        } catch (ClassCastException | NullPointerException e) {
+            throw new AuthorizationRequiredException(messages.get("reksoft.demo.auth.filter.credentialsNotFound.message"), e);
+        }
+    }
+
+    /**
+     * Returns authenticated user.
+     *
+     * @return user entity
+     * @throws AuthorizationRequiredException - if user is anonymous
+     */
+    protected UserEntity getCurrentUserEntity() throws AuthorizationRequiredException {
+        return userRepository.getOne(getCurrentUserId());
+    }
+}
