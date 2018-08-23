08b907e76e8dcc7f6979a75478a7de7d9ac4e0a4
Artiow <namednev_a@mail.ru>
2018-08-23 18:01:21 +0300

    reservation mapper fix

--- src/main/java/ru/vldf/sportsportal/mapper/sectional/lease/ReservationMapper.java
+++ src/main/java/ru/vldf/sportsportal/mapper/sectional/lease/ReservationMapper.java
@@ -5,8 +5,9 @@ import org.mapstruct.Mapping;
 import ru.vldf.sportsportal.domain.sectional.lease.ReservationEntity;
 import ru.vldf.sportsportal.dto.sectional.lease.ReservationDTO;
 import ru.vldf.sportsportal.mapper.generic.ObjectMapper;
+import ru.vldf.sportsportal.mapper.manual.JavaTimeMapper;
 
-@Mapper
+@Mapper(componentModel = "spring", uses = JavaTimeMapper.class)
 public interface ReservationMapper extends ObjectMapper<ReservationEntity, ReservationDTO> {
 
     @Mapping(target = "datetime", source = "pk.datetime")
