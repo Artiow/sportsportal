4b05a3daa0f6d6cb5465aa163cd34f90d89bd6d3
Artiow <namednev_a@mail.ru>
2018-08-24 18:39:26 +0300

    tests template add

---
+++ src/test/java/ru/vldf/sportsportal/mapper/PlaygroundMapperTests.java
@@ -0,0 +1,19 @@
+package ru.vldf.sportsportal.mapper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class PlaygroundMapperTests {
+
+    @Test
+    public void prepareGrid() {
+
+    }
+
+    @Test
+    public void setGrid() {
+
+    }
+}
