84c0d2d3f0f28a0a10cd1dce7b451e9c7c1bd99b
Artiow <namednev_a@mail.ru>
2018-09-03 16:50:29 +0300

    libs downloaded

--- src/main/java/ru/vldf/sportsportal/config/ApplicationConfig.java
+++ src/main/java/ru/vldf/sportsportal/config/ApplicationConfig.java
@@ -1,17 +1,9 @@
 package ru.vldf.sportsportal.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class ApplicationConfig implements WebMvcConfigurer {
 
-    /**
-     * Resource handlers configuration.
-     */
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-
-    }
 }
