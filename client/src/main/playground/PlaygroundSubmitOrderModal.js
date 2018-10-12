5eeb73689b5be11d6f62d8b2d21884ce90862518
Artiow <namednev_a@mail.ru>
2018-10-12 10:31:59 +0300

    submit table template add

--- client/src/main/playground/PlaygroundSubmitOrderModal.js
+++ client/src/main/playground/PlaygroundSubmitOrderModal.js
@@ -1,11 +1,41 @@
 import React from 'react';
+import {env} from '../../boot/constants';
+import './PlaygroundSubmitOrderModal.css';
 
 export default function PlaygroundSubmitOrderModal(props) {
 
     const HEADER_TITLE = 'Подтвердите правильность выбора';
 
+    const rawSchedule = new Map();
+    const a = [];
+    props.reservation.forEach(value => {
+        const datetime = value.split('T');
+        const date = datetime[0];
+        const time = datetime[1];
+        if (rawSchedule.has(date)) rawSchedule.get(date).push(time);
+        else rawSchedule.set(date, [time]);
+    });
+
+    const table = [];
+    const headerLine = [];
+    rawSchedule.forEach(value => value.sort());
+    const schedule = Array.from(rawSchedule).sort();
+    schedule.forEach((value, index) => {
+        const date = value[0];
+        const dateClass = new Date(date);
+        headerLine.push(
+            <th key={index} className="th-calendar">
+                <div className="small">{env.MONTH_NAMES[dateClass.getMonth()]}</div>
+                <div>
+                    <span className="mr-1">{date.split('-')[2]}</span>
+                    <span className="ml-1">{env.DAYS_OF_WEEK_NAMES[dateClass.getDay()].short.toUpperCase()}</span>
+                </div>
+            </th>
+        );
+    });
+
     return (
-        <div id={props.submitId} className="PlaygroundOrderModal modal fade" tabIndex="-1">
+        <div id={props.submitId} className="PlaygroundSubmitOrderModal modal fade" tabIndex="-1">
             <div className="modal-dialog">
                 <div className="modal-content">
                     <div className="modal-header">
@@ -13,7 +43,13 @@ export default function PlaygroundSubmitOrderModal(props) {
                         <button type="button" className="close" data-dismiss="modal"><span>&times;</span></button>
                     </div>
                     <div className="modal-body">
-                        ...
+                        <table className="table table-hover">
+                            <thead className="thead-light">
+                            <tr>{headerLine}</tr>
+                            </thead>
+                            <tbody>
+                            </tbody>
+                        </table>
                     </div>
                     <div className="modal-footer">
                         <button type="button" className="btn btn-secondary" data-dismiss="modal">
