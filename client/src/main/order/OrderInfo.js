7b29480d42834d65aba40c950cb4207546bb3ec1
Artiow <namednev_a@mail.ru>
2018-10-17 19:28:49 +0300

    order page template update

--- client/src/main/order/OrderInfo.js
+++ client/src/main/order/OrderInfo.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import apiUrl from '../../boot/constants';
+import {withFrameContext} from '../../boot/frame/MainFrame';
 import axios from 'axios';
 import './OrderInfo.css';
-import {withFrameContext} from "../../boot/frame/MainFrame";
 
 export default withFrameContext(class OrderInfo extends React.Component {
+
+    static UUID_LENGTH = 10;
+
     constructor(props) {
         super(props);
         this.identifier = this.props.identifier;
@@ -35,12 +38,50 @@ export default withFrameContext(class OrderInfo extends React.Component {
     }
 
     render() {
-        return (
+        const orderId = id => {
+            let result = '' + id;
+            while (result.length < OrderInfo.UUID_LENGTH)
+                result = '0' + result;
+            return result;
+        };
+        const orderStatus = (paid, owner) => {
+            let text = 'ошибка';
+            let styleClass = 'danger';
+            if (paid && owner) {
+                text = 'зарезервировано';
+                styleClass = 'primary';
+            } else if (paid && !owner) {
+                text = 'оплачено';
+                styleClass = 'success';
+            } else if (!paid && !owner) {
+                text = 'не оплачено';
+                styleClass = 'warning';
+            }
+            return (<span className={`badge badge-${styleClass}`}>{text}</span>)
+        };
+        const order = this.state.content;
+        return order ? (
             <div className="OrderInfo row">
                 <div className="col-10 offset-1">
-                    <code>{JSON.stringify(this.state.content)}</code>
+                    <div className="container content-container">
+                        <div className="row header-row">
+                            <div className="col-12">
+                                <h1 className="row-h header-h header-name">
+                                    {`Заказ #${orderId(order.id)}`}
+                                </h1>
+                                <h4 className="row-h header-h header-address">
+                                    статус: {orderStatus(order.paid, order.byOwner)}
+                                </h4>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-12">
+                                <code>{JSON.stringify(this.state.content)}</code>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        );
+        ) : (null);
     }
 })
