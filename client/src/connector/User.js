f96f14a5c3288dfb642dd2bf525a707e4b8cb350
Artiow <namednev_a@mail.ru>
2018-10-30 15:32:54 +0300

    user connector add, order connector fix

--- client/src/connector/User.js
+++ client/src/connector/User.js
@@ -2,28 +2,28 @@ import Authentication from './Authentication';
 import apiUrl from '../boot/constants';
 import axios from 'axios';
 
-export default class Order {
+export default class User {
 
     static get(id) {
         return new Promise((resolve, reject) => {
             Authentication.access()
                 .then(token => {
                     axios
-                        .get(apiUrl('/order/' + id), {
+                        .get(apiUrl('/user/' + id), {
                             headers: {Authorization: `Bearer ${token}`}
                         })
                         .then(response => {
-                            console.debug('Order [get]:', response);
+                            console.debug('User [get]:', response);
                             resolve(response.data);
                         })
                         .catch(error => {
                             const response = error.response;
-                            console.error('Order [get]:', response ? response : error);
+                            console.error('User [get]:', response ? response : error);
                             reject((response && response.data) ? response.data : null)
                         })
                 })
                 .catch(error => {
-                    console.error('Order [get]: access error');
+                    console.error('User [get]: access error');
                 });
         });
     }
@@ -33,21 +33,21 @@ export default class Order {
             Authentication.access()
                 .then(token => {
                     axios
-                        .delete(apiUrl('/order/' + id), {
+                        .delete(apiUrl('/user/' + id), {
                             headers: {Authorization: `Bearer ${token}`}
                         })
                         .then(response => {
-                            console.debug('Order [delete]:', response);
+                            console.debug('User [delete]:', response);
                             resolve();
                         })
                         .catch(error => {
                             const response = error.response;
-                            console.error('Order [delete]:', response ? response : error);
+                            console.error('User [delete]:', response ? response : error);
                             reject((response && response.data) ? response.data : null)
                         })
                 })
                 .catch(error => {
-                    console.error('Order [delete]: access error');
+                    console.error('User [delete]: access error');
                 });
         });
     }
