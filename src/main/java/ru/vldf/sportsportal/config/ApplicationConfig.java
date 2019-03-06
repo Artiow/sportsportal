c7408c6a60ed05dc42548cf9d243b4eeb70415f9
Artiow <namednev_a@mail.ru>
2019-03-06 12:31:42 +0300

    max file uploading parameter add, javadoc update

--- src/main/java/ru/vldf/sportsportal/config/ApplicationConfig.java
+++ src/main/java/ru/vldf/sportsportal/config/ApplicationConfig.java
@@ -1,7 +1,11 @@
 package ru.vldf.sportsportal.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,6 +15,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class ApplicationConfig implements WebMvcConfigurer {
 
+    @Value("${server.upload-size.max:1048576}")
+    private long maxUploadSize;
+
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry
@@ -18,5 +26,13 @@ public class ApplicationConfig implements WebMvcConfigurer {
                 .exposedHeaders(HttpHeaders.LOCATION)
                 .allowedMethods("*")
                 .allowedOrigins("*");
+    }
+
+
+    @Bean
+    public MultipartResolver multipartResolver() {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setMaxUploadSize(maxUploadSize);
+        return multipartResolver;
     }
 }
