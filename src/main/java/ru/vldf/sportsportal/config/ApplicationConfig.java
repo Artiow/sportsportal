c8254946248bdb396f245f911c18ae89f43d9dd4
Artiow <namednev_a@mail.ru>
2019-03-08 16:01:57 +0300

    forwarded header filter add

--- src/main/java/ru/vldf/sportsportal/config/ApplicationConfig.java
+++ src/main/java/ru/vldf/sportsportal/config/ApplicationConfig.java
@@ -1,7 +1,10 @@
 package ru.vldf.sportsportal.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,6 +13,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class ApplicationConfig implements WebMvcConfigurer {
+
+    @Bean
+    public FilterRegistrationBean<ForwardedHeaderFilter> forwardedHeaderFilter() {
+        FilterRegistrationBean<ForwardedHeaderFilter> bean = new FilterRegistrationBean<>();
+        bean.setFilter(new ForwardedHeaderFilter());
+        return bean;
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
