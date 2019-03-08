6d2485fdd2b04a1b71b27c7683343e6593f4b1a7
Artiow <namednev_a@mail.ru>
2019-03-08 16:14:18 +0300

    javadoc update

--- src/main/java/ru/vldf/sportsportal/util/Base64Credentials.java
+++ src/main/java/ru/vldf/sportsportal/util/Base64Credentials.java
@@ -1,27 +1,22 @@
 package ru.vldf.sportsportal.util;
 
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.util.Assert;
 import org.springframework.util.Base64Utils;
 
-import javax.validation.constraints.NotBlank;
 import java.nio.charset.StandardCharsets;
 
 /**
  * @author Namednev Artem
  */
 @Getter
-@RequiredArgsConstructor(staticName = "of")
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE, staticName = "of")
 public final class Base64Credentials {
 
-    @NotBlank
     private final String username;
-
-    @NotBlank
     private final String password;
-
-    private transient String credentials = null;
 
     /**
      * Decode Base64 credentials (like {@literal username:password}).
@@ -31,13 +26,9 @@ public final class Base64Credentials {
      * @throws IllegalArgumentException if credentials has not valid format.
      */
     public static Base64Credentials decode(String credentials) throws IllegalArgumentException {
-        Assert.hasText(credentials, "credentials must not bu blank");
+        Assert.hasText(credentials, "credentials must not be blank");
         String[] arr = new String(Base64Utils.decodeFromString(credentials), StandardCharsets.UTF_8).split(":", 2);
         if (arr.length == 2) return Base64Credentials.of(arr[0], arr[1]);
         else throw new IllegalArgumentException("invalid base64 credentials format");
-    }
-
-    public String encode() {
-        return credentials == null ? credentials = username + ":" + password : credentials;
     }
 }
