086cacf7caf59d16aa104842b2d48836fb20dd64
Artiow <namednev_a@mail.ru>
2019-04-10 17:50:33 +0300

    links refactoring (part 2)

--- src/main/java/ru/vldf/sportsportal/service/dictionary/common/PictureSizeService.java
+++ src/main/java/ru/vldf/sportsportal/service/dictionary/common/PictureSizeService.java
@@ -2,7 +2,7 @@ package ru.vldf.sportsportal.service.dictionary.common;
 
 import org.springframework.stereotype.Service;
 import ru.vldf.sportsportal.domain.sectional.common.PictureSizeEntity;
-import ru.vldf.sportsportal.dto.sectional.common.PictureSizeDTO;
+import ru.vldf.sportsportal.dto.sectional.common.specialized.PictureSizeDTO;
 import ru.vldf.sportsportal.service.dictionary.AbstractDictionaryService;
 
 /**
