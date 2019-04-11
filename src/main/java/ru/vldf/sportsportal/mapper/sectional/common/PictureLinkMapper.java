ad43a60e73f73825f424badad4c7c92893abc5a4
Artiow <namednev_a@mail.ru>
2019-04-11 13:02:45 +0300

    mapping fix

--- src/main/java/ru/vldf/sportsportal/mapper/sectional/common/PictureLinkMapper.java
+++ src/main/java/ru/vldf/sportsportal/mapper/sectional/common/PictureLinkMapper.java
@@ -14,6 +14,6 @@ import ru.vldf.sportsportal.mapper.manual.url.common.PictureURLMapper;
 @Mapper(componentModel = "spring", uses = {PictureURLMapper.class})
 public abstract class PictureLinkMapper implements LinkMapper<PictureEntity, PictureLinkDTO> {
 
-    @Mapping(target = "url", source = "id", qualifiedByName = {"toPictureURL", "fromId"})
+    @Mapping(target = "pictureURL", source = "id", qualifiedByName = {"toPictureURL", "fromId"})
     public abstract PictureLinkDTO toLinkDTO(PictureEntity entity);
 }
