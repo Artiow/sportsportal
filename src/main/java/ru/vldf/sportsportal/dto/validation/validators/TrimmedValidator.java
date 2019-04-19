b52d11c7794681a308f80ddfee68488f3bfbdf61
Artiow <namednev_a@mail.ru>
2019-04-19 18:33:27 +0300

    add trimmed string validator

---
+++ src/main/java/ru/vldf/sportsportal/dto/validation/validators/TrimmedValidator.java
@@ -0,0 +1,19 @@
+package ru.vldf.sportsportal.dto.validation.validators;
+
+import ru.vldf.sportsportal.dto.validation.annotations.Trimmed;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+/**
+ * @author Artem Namednev
+ */
+public class TrimmedValidator implements ConstraintValidator<Trimmed, String> {
+
+    @Override
+    public boolean isValid(String s, ConstraintValidatorContext context) {
+        if (s == null) return true;
+        char[] arr = s.toCharArray();
+        return (arr[0] > ' ') && (arr[arr.length - 1] > ' ');
+    }
+}
